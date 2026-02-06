a024982b961eca60ccbc9b16f5a4f6147c9b2269
Zeotical <aceyems10@gmail.com>
2026-02-06 13:59:45 +0800

    Complex multer configs

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 console.log('hello world')
 const cors = require('cors');
-
 const path = require('path');
+const multer = require('multer');
 
+// const fileStorage = multer.diskStorage({
+// destination: (req, file, cb) => {
+// cb(null, 'images'); },
+// filename: (req, file, cb) => {
+// cb(null, new Date().toISOString() + '-' + file.originalname); //or cb(null, new Date().toISOString() + '-' + file.originalname); 
+// }
+// });
+
+// const fileFilter = (req, file, cb) => {
+// if (
+// file.mimetype === 'publication_file/pdf' 
+// ) { cb(null, true) }
+// else {
+// cb(null, false); } 
+// }
+
+const upload = multer({dest: 'static/publications'});
 //express set up 
 const express = require('express');
 const app = express();
 app.use(cors()) ; //allow access from any ip
 app.use('/static', express.static('static')); //serving files from static
 app.use(express.json()); //converts raw JSON data it into a usable js obj
+
+// app.use(multer({storage: fileStorage, fileFilter:fileFilter }).single('publication_file'));
 
 //db setup
 const sqlite3= require('sqlite3').verbose();
@@ -26,6 +45,7 @@ if (err) return console.error(err.message); // rpms == research publication syst
 // db.run(dropsql);
 // dropsql = 'DROP TABLE proof'; // alt db.run("DROP TABLE users");
 // db.run(dropsql);
+
 //enable forgien keys
 db.run("PRAGMA foreign_keys = ON");
 
@@ -48,6 +68,7 @@ title,
 status,
 researcherID INTEGER, 
 publicationDate DATE,
+publicationFilePath,
 CONSTRAINT FK_researcher_id FOREIGN KEY (researcherID) REFERENCES users(id)
 )`;
 db.run(publicationTable_sql);
@@ -57,6 +78,7 @@ proofTable_sql =
 proofID INTEGER PRIMARY KEY, 
 publicationID INTEGER, 
 uploadDate DATE,
+proofFilePath,
 CONSTRAINT FK_publication_id FOREIGN KEY (publicationID) REFERENCES publication(publicationID)
 )`;
 db.run(proofTable_sql);
@@ -118,6 +140,45 @@ res.sendFile(path.join(__dirname, 'templates', 'progcoordinator.html'));
 //Researcher Route (GET + POST)
 app.get('/researcher', (req, res) => {
 res.sendFile(path.join(__dirname, 'templates', 'researcher.html'));
+})
+
+app.post('/researcher' ,upload.any(), (req, res) => {
+    const title = req.body;
+    const publication_file = req.file ;
+//     if(!publication_file) {
+//         return res.status(422).render('researcher', {
+//         pageTitle: 'Researcher Dashboard',
+//         path: '/researcher',
+//         editing: false,
+//         hasError: true,
+//         product: {
+//         title: title,
+//         // description: description 
+//         },
+//         errorMessage: 'Attached file is not a pdf.',
+//         validationErrors: []
+// });
+// }
+
+const publication_file_path = publication_file.path;
+
+//     const { username, ps } = req.body;
+// //query the data
+//  auth_sql = 'SELECT * FROM users WHERE username = ?';
+//  db.get(auth_sql, [username], (err, row) => {
+//  if (err) return console.error("User not found");
+//  else if (row.password == ps) res.send(row.role)
+// });
+//Insert into table
+i_sql =  'INSERT INTO publication(researcherID, title, publicationDate, publicationFilePath) VALUES (?,?,?)';
+db.run(i_sql, ["id", title,"date",publication_file_path] ,(err) => {
+if (err) {
+    console.error(err.message);
+    return res.status(500).send('Database error');
+}
+ res.send('Publication Uploaded'); //send response
+
+})
 })
 
 //Student Route (GET + POST)
